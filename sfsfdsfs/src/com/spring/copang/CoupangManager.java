c7785670d846f18c9df3cfb33f63110f74662f67
ung <ung6039@naver.com>
2020-05-11 17:59:00 +0900

    commit

--- sfsfdsfs/src/com/spring/copang/CoupangManager.java
+++ sfsfdsfs/src/com/spring/copang/CoupangManager.java
@@ -12,11 +12,14 @@ public class CoupangManager {
 	public List<CoupangVO> sListData(){
 		List<CoupangVO> list = new ArrayList<CoupangVO>();
 		int page=1;
-		
 //		for(page =1; page<=17; page++){
 			try{
+				System.out.println("!!");
+				Document doc2 = Jsoup.connect("https://www.coupang.com/").get();
+				System.out.println(doc2.toString());
+				System.out.println("??");
 				Document doc = Jsoup.connect("https://www.coupang.com/np/categories/195268?listSize=60&brand=&offerCondition=&filterType=&isPriceRange=false&minPrice=&maxPrice=&page=1&channel=user&fromComponent=Y&selectedPlpKeepFilter=&sorter=bestAsc&filter=&component=195168&rating=0").get();
-				
+				System.out.println(doc.toString());
 				Elements name = doc.select("dl.baby-product-wrap dd.descriptions div.name");
 				Elements image = doc.select("dl.baby-product-wrap dt.image img");
 				Elements discount_percentage = doc.select("dl.baby-product-wrap dd.descriptions div.price-area div> span.price-info span.discount-percentage");
@@ -39,6 +42,6 @@ public class CoupangManager {
 	public static void main(String[] args) {
 		
 		CoupangManager sm = new CoupangManager();
-		sm.sListData();
+		List<CoupangVO> list = sm.sListData();
 	}
 }
