2be1bb7201a5ff94f85beec178c2ffbbf24f04ae
ung <ung6039@naver.com>
2020-05-11 18:14:53 +0900

    크롤링 문제 해결!

--- sfsfdsfs/src/com/spring/copang/CoupangManager.java
+++ sfsfdsfs/src/com/spring/copang/CoupangManager.java
@@ -15,11 +15,8 @@ public class CoupangManager {
 //		for(page =1; page<=17; page++){
 			try{
 				System.out.println("!!");
-				Document doc2 = Jsoup.connect("https://www.coupang.com/").get();
-				System.out.println(doc2.toString());
-				System.out.println("??");
-				Document doc = Jsoup.connect("https://www.coupang.com/np/categories/195268?listSize=60&brand=&offerCondition=&filterType=&isPriceRange=false&minPrice=&maxPrice=&page=1&channel=user&fromComponent=Y&selectedPlpKeepFilter=&sorter=bestAsc&filter=&component=195168&rating=0").get();
-				System.out.println(doc.toString());
+				Document doc = Jsoup.connect("https://www.coupang.com/np/categories/195268?listSize=60&brand=&offerCondition=&filterType=&isPriceRange=false&minPrice=&maxPrice=&page=1&channel=user&fromComponent=Y&selectedPlpKeepFilter=&sorter=bestAsc&filter=&component=195168&rating=0")
+						.post();
 				Elements name = doc.select("dl.baby-product-wrap dd.descriptions div.name");
 				Elements image = doc.select("dl.baby-product-wrap dt.image img");
 				Elements discount_percentage = doc.select("dl.baby-product-wrap dd.descriptions div.price-area div> span.price-info span.discount-percentage");
