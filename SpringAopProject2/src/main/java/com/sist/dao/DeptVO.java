15191eecbba7e04b0258828a0d6cc32948f6f6fb
ung <ung6039@naver.com>
2020-05-19 17:24:06 +0900

    AOP

---
+++ SpringAopProject2/src/main/java/com/sist/dao/DeptVO.java
@@ -0,0 +1,14 @@
+package com.sist.dao;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DeptVO {
+	
+	private int deptno;
+	private String dname;
+	private String loc;
+	
+}
